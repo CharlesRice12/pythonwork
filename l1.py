1e9856019e296c8a2d73edc1055bf6156a7aa84a
Coderev <61511894+codere109@users.noreply.github.com>
2021-03-01 22:54:23 -0500

    Add files via upload
    Just wanted you to give feedback on this loop, regarding the understanding level and the approach as far as using the syntax.

---
+++ l1.py
@@ -0,0 +1,19 @@
+highSchool = "chase"
+chaseHrs = "eightHrs"
+eightHrs = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm"]
+fullDay = eightHrs
+earlyDismissal = "School lets out at 11"
+
+for time in eightHrs:
+    print("eightHrs")
+if (highSchool =='12pm'):
+    print("School lets out at 2")
+    
+
+else:
+    print(earlyDismissal)
+
+    
+
+    
+    
